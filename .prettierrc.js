c4793f745e902b2c47839eee4f7fbc943ec7103c
Douglas Rodrigues <douglas.r129@outlook.com>
2023-07-26 17:51:43 -0300

    config styled component

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-	htmlWhitespaceSensitivity: 'ignore',
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'all',
-	embeddedLanguageFormatting: 'off',
+  htmlWhitespaceSensitivity: 'ignore',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  embeddedLanguageFormatting: 'off',
 };
